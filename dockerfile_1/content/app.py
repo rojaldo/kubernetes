850835a8c0fd56ffdf5c836ba00b19da5b6c96bc
Daniel Rojo <danielrojo@gmail.com>
2025-05-13 12:38:03 +0200

    ollama

--- dockerfile_1/content/app.py
+++ dockerfile_1/content/app.py
@@ -2,9 +2,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+# this route 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 app.run( host='0.0.0.0', port=5000, debug=True)
+
+
 
